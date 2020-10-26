c7e3e7b5d152930f104f392c781637a705cb151e
Kamil Piekarski <piekarskikamil97@gmail.com>
2020-10-27 00:17:01 +0100

    lab3zad4

---
+++ Assets/Scripts/CollisionDetection.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioData;
+    void Start()
+    {
+        var cubeRenderer = gameObject.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+            Debug.Log("BUM! Kolizja");
+            audioData.Play(0);
+        }
+        else
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+    }
+}
